2ef20b03794f497453626414e93c64b3ffde541b
Asumji <64544378+Asumji@users.noreply.github.com>
2022-11-25 18:44:07 +0100

    forgot what I added
    I definitly did not forget to keep github up-to-date :)

--- Bread/index.js
+++ Bread/index.js
@@ -2,7 +2,7 @@
 import PogObject from "PogData";
 
 const data = new PogObject("Bread", {
-  names: ["&r&azhadow_t"],
+  names: ["zhadow_t"],
 });
 data.save();
 
@@ -10,13 +10,13 @@ register("Chat", function(event) {
   let msg = ChatLib.getChatMessage(event, true)
   for (let i = 0; i < data.names.length; i++) {
     if (msg.toLowerCase().includes(data.names[i])) {
-      msgReplaced = msg.replace("and became a ghost.", "and went to get bread.")
+      msgReplaced = msg.replace("and became a ghost", "and went to get bread")
       cancel(event)
       ChatLib.chat(msgReplaced)
       break;
     }
   }
-}).setChatCriteria("became a ghost.").setContains();
+}).setChatCriteria("became a ghost&r&7.&r").setContains();
 
 register("command", (...args) => {
   if (args[0] == "add") {
@@ -29,7 +29,7 @@ register("command", (...args) => {
         ChatLib.chat("§cName is already being checked.")
       }
     } else {
-      ChatLib.chat("§cDidn't provide an Name.")
+      ChatLib.chat("§cDidn't provide a Name.")
     }
   } else if (args[0] == "remove") {
     if (args[1]) {
@@ -41,7 +41,7 @@ register("command", (...args) => {
         ChatLib.chat("§cName isn't being checked.")
       }
     } else {
-      ChatLib.chat("§cDidn't provide an Name.")
+      ChatLib.chat("§cDidn't provide a Name.")
     }
   } else if (args[0] == "list") {
     string = "§2Mod currently checks for:\n"
